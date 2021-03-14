3ec582586a48d911d4d4544d51e8a5ea30bd8d22
Derek Brigner <derek.brigner@ustrotting.com>
2021-03-14 15:39:55 -0400

    Remove branding

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
       <header className="flex flex-col lg:flex-row w-full bg-white">
         <div className="flex flex-row w-full lg:w-3/12">
           <div className="w-5/6 lg:w-4/12 p-3 text-gray-600 text-xl">Brand</div>
-          <h1 className="hidden lg:block lg:w-8/12 p-3 text-gray-600 text-xl text-center">U.S. Trotting Association</h1>
+          <h1 className="hidden lg:block lg:w-8/12 p-3 text-gray-600 text-xl text-center">Organization Name</h1>
           <div className="flex justify-center w-1/6 p-3 lg:hidden">
             <FontAwesomeIcon icon={faBars} className="h-8 text-gray-400" onClick={handleHide} />
           </div>
@@ -25,9 +25,8 @@ function MyApp({ Component, pageProps }) {
         <nav className={`w-full lg:w-9/12 ${hide} lg:block`}>
           <ul className="flex flex-col lg:flex-row">
             <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">News</a></li>
-            <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">Racing</a></li>
-            <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">HoofBeats</a></li>
-            <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">Members</a></li>
+            <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">About</a></li>
+            <li className="w-full p-4 uppercase hover:bg-gray-100"><a href="" className="hover:underline">Contact</a></li>
             <li className="flex flex-row w-full">
               <input type="text" name="searchTxt" className="w-3/4 p-2 focus:border-0" placeholder="Search"></input>
               <button name="searchBtn" className="flex items-center justify-center w-1/4 p-2 bg-green-600 text-white text-center" type="submit">
@@ -48,33 +47,32 @@ function MyApp({ Component, pageProps }) {
             <div className="m-2">
               <a href="https://goo.gl/maps/gCkEFHreA7giGvp17" target="_blank" className="hover:underline">
                 <div className="flex flex-col">
-                  <div>United States Trotting Association</div>
-                  <div>6130 Sunbury Rd.,</div>
-                  <div>Westerville, Ohio 43081</div>
+                  <div>Name</div>
+                  <div>123 Address Rd.,</div>
+                  <div>City, State 12345</div>
                 </div>
               </a>
             </div>
             <div className="m-2">
-              <div>Phone: <a href="tel:1-877-800-8782">1-877-800-USTA</a></div>
+              <div>Phone: <a href="tel:1-555-555-5555">1-555-555-5555</a></div>
               <div>Hours: Mon. - Fri. 8 A.M. - 4:30 P.M. EST</div>
             </div>
           </div>
           <div className="w-full p-2">
-            <h2 className="font-bold text-2xl text-gray-300 uppercase">Join the USTA</h2>
+            <h2 className="font-bold text-2xl text-gray-300 uppercase">Join Us</h2>
             <div className="m-2">
-              <div><a href="http://members.ustrotting.com" className="hover:underline">What is the USTA?</a></div>
-              <div><a href="http://members.ustrotting.com/about.cfm" className="hover:underline">Become a Member</a></div>
-              <div><a href="http://members.ustrotting.com/membership.cfm" className="hover:underline">Renew Membership</a></div>
-              <div><a href="http://members.ustrotting.com/member_discounts.cfm" className="hover:underline">Member Discounts</a></div>
-              <div><a href="http://myaccount.ustrotting.com/" className="hover:underline">Free Online Account</a></div>
+              <div><a className="hover:underline">Who are we?</a></div>
+              <div><a className="hover:underline">Become a Member</a></div>
+              <div><a className="hover:underline">Renew Membership</a></div>
+              <div><a className="hover:underline">Member Discounts</a></div>
+              <div><a className="hover:underline">Free Account</a></div>
             </div>
           </div>
           <div className="w-full p-2">
             <h2 className="font-bold text-2xl text-gray-300 uppercase">Customer Service</h2>
             <div className="m-2">
-              <div><a href="http://members.ustrotting.com/" className="hover:underline">Member Services</a></div>
-              <div><a href="http://www.ustrotting.com/contact.cfm" className="hover:underline">Department Directory</a></div>
-              <div><a href="http://myaccount.ustrotting.com" className="hover:underline">MyAccount</a></div>
+              <div><a className="hover:underline">Member Services</a></div>
+              <div><a className="hover:underline">Department Directory</a></div>
             </div>
           </div>
           <div className="w-full p-2">
@@ -85,12 +83,12 @@ function MyApp({ Component, pageProps }) {
               <FontAwesomeIcon icon={faYoutube} />
             </div>
             <h2 className="font-bold text-2xl text-gray-300 uppercase">Subscribe</h2>
-            <div className="m-2"><a href="http://hoofbeats.ustrotting.com" className="hover:underline">Hoof Beats Magazine</a></div>
+            <div className="m-2"><a className="hover:underline">Organization Magazine</a></div>
           </div>
           <div className="w-full"></div>
         </div>
         <div className="p-4 text-center text-white">
-          ©2020 The United States Trotting Association | <a>Privacy Policy</a> | <a>Terms of Use</a>
+          ©2020 Organization Name | <a>Privacy Policy</a> | <a>Terms of Use</a>
         </div>
       </footer>
     </div>
